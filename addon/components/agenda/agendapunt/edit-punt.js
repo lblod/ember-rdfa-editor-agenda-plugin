ad3db34fa6b18d963466748a662ded00bf1ad1a9
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-01-31 18:23:18 +0100

    some workarounds for data which is not properly casted

--- addon/components/agenda/agendapunt/edit-punt.js
+++ addon/components/agenda/agendapunt/edit-punt.js
@@ -1,6 +1,17 @@
 import Component from '@ember/component';
 import layout from '../../../templates/components/agenda/agendapunt/edit-punt';
-
+import { computed } from '@ember/object';
 export default Component.extend({
-  layout
+  layout,
+
+  //some casting is needed, should be fixed in generic-model-plugin-utils
+  geplandOpenbaar: computed('agendapunt.geplandOpenbaar', function(){
+    return this.agendapunt.geplandOpenbaar == 'true' || this.agendapunt.geplandOpenbaar == true;
+  }),
+
+  actions: {
+    toggleGeplandOpenbaar(){
+      this.agendapunt.set('geplandOpenbaar', !this.agendapunt.geplandOpenbaar);
+    }
+  }
 });
