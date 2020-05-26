fb674df1ee1cf72c6520228779fc5f7ae50c0584
Erika Pauwels <erika.pauwels@gmail.com>
2020-05-26 17:05:10 +0200

    Ignore functions on ember object copy

--- addon/utils/copy-ember-object.js
+++ addon/utils/copy-ember-object.js
@@ -3,7 +3,7 @@ import EmberObject from '@ember/object';
 export default function copyEmberObject(obj) {
   const result = {};
   Object.keys(obj)
-    .filter( (k) => k !== "_super" )
+    .filter( (k) => !(typeof obj[k] === 'function'))
     .forEach( (k) => result[k] = obj[k] );
   return EmberObject.create( result );
 }
